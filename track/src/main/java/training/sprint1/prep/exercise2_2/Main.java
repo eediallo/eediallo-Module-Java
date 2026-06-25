e4218615ac79d0a460d9706e7428612d8bf3cb66
eedialllo <elhhabdouldiallo@gmail.com>
2026-06-25 22:48:31 +0100

    add solution for exercise2_2

---
+++ track/src/main/java/training/sprint1/prep/exercise2_2/Main.java
@@ -0,0 +1,16 @@
+package training.sprint1.prep.exercise2_2;
+
+/*
+*✍️Exercise 2.2
+Goal: Identify and fix compilation errors.
+
+Remove a semicolon or misspell a keyword in your code.
+Observe the compilation error in IntelliJ.
+Fix the error and re-run the program
+* */
+
+public class Main {
+    public static void main(String[] args) {
+        String fullName = "Micheal Jones";
+    }
+}
