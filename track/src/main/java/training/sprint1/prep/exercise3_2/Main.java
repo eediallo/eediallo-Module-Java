c9212f0ca4a3a1d71e7f2ef9d128ff3fb631665f
eedialllo <elhhabdouldiallo@gmail.com>
2026-06-26 00:13:54 +0100

    exercise3_2: add JavaDoc to explain the method.

--- track/src/main/java/training/sprint1/prep/exercise3_2/Main.java
+++ track/src/main/java/training/sprint1/prep/exercise3_2/Main.java
@@ -8,6 +8,13 @@ Add JavaDoc comments explaining the method.
 
 public class Main {
 
+    /**
+     * Calculate the sum of two integers
+     *
+     * @param num1 the first integer to add.
+     * @param num2 the second integer to add.
+     * @return the arithmetic of num1 and num2.
+     */
     public int calculateSum(int num1, int num2) {
         return num1 + num2;
     }
