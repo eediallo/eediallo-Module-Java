01a132d1ebdc8d50430f0446c85fbfce2bdae477
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-17 17:44:25 +0100

    add square method and related test.

---
+++ track/sprint3/src/test/java/prep/exercise1_2/CompleteCalculator.java
@@ -0,0 +1,9 @@
+package prep.exercise1_2;
+
+import prep.exercise1_1.Calculator;
+
+public class CompleteCalculator extends Calculator {
+    public int square(int a){
+        return a * a;
+    }
+}
---
+++ track/sprint3/src/test/java/prep/exercise1_2/CompleteCalculatorTest.java
@@ -0,0 +1,21 @@
+package prep.exercise1_2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompleteCalculatorTest {
+    private CompleteCalculator completeCalculator;
+
+    @BeforeEach
+    void setup(){
+        completeCalculator = new CompleteCalculator();
+    }
+
+    @Test
+    void testSquare(){
+        assertEquals(25, completeCalculator.square(5), "5 * 5 = 25");
+    }
+
+}
