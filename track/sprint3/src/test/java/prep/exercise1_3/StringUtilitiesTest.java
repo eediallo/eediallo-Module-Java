4149c2a777e177646e11249a5872ee6c19bc55e2
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-18 09:02:07 +0100

    add tests for isPalindrome

---
+++ track/sprint3/src/test/java/prep/exercise1_3/StringUtilitiesTest.java
@@ -0,0 +1,22 @@
+package prep.exercise1_3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilitiesTest {
+
+    private StringUtilities stringUtilities;
+
+    @BeforeEach
+    void setup() {
+        stringUtilities = new StringUtilities();
+    }
+
+    @Test
+    void testisPalindrome() {
+        assertAll(() -> assertTrue(stringUtilities.isPalindrome("civic"), "civic is a Palindrome"), () -> assertTrue(stringUtilities.isPalindrome("radar")), () -> assertTrue(stringUtilities.isPalindrome("level")), () -> stringUtilities.isPalindrome("rotor"), () -> assertTrue(stringUtilities.isPalindrome("madam")));
+
+    }
+}
