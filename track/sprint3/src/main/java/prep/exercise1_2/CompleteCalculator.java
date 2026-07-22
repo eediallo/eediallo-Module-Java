ec94027c993d78b59cf6036603aa7483943cf55a
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-22 22:09:00 +0100

    add javadoc for the CompleteCalculator class and its method.

--- track/sprint3/src/main/java/prep/exercise1_2/CompleteCalculator.java
+++ track/sprint3/src/main/java/prep/exercise1_2/CompleteCalculator.java
@@ -2,11 +2,28 @@ package prep.exercise1_2;
 
 import prep.exercise1_1.Calculator;
 
+/**
+ * An extension of the basic {@link Calculator} that adds support for
+ * advanced mathematical operations such as squaring and square roots
+ */
 public class CompleteCalculator extends Calculator {
+    /**
+     * Calculate the square of an integer (a value multiply by itself).
+     *
+     * @param a the value to be squared
+     * @return the product of the {@code a * a}
+     */
     public int square(int a) {
         return a * a;
     }
 
+    /**
+     * Calculates the principal square root of a non-negative integer.
+     *
+     * @param a the non-negative integer whose square root is to be calcualted
+     * @return the positive square root of {@code a} as {@code double}
+     * @throws IllegalArgumentException if {@code a} is negative
+     */
     public double squareRoot(int a) {
         if (a < 0) {
             throw new IllegalArgumentException("Square root of a negative number");
