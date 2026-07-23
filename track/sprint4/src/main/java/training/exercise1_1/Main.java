fd1e598cd56820e211160226682f892bede51c07
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-23 09:07:37 +0100

    update main to use the StatementService.

--- track/sprint4/src/main/java/training/exercise1_1/Main.java
+++ track/sprint4/src/main/java/training/exercise1_1/Main.java
@@ -1,12 +1,23 @@
 package training.exercise1_1;
+
+import training.exercise1_2.StatementService;
+
+import java.time.LocalDate;
+
 /**Entry point demonstrator for statement deliveries using different channels.*/
 public class Main {
     public static void main(String[] args) {
         StatementDeliveryService statementDeliveryService = new StatementDeliveryService();
+        StatementService statementService = new StatementService(statementDeliveryService);
 
-        // send via Email
-        statementDeliveryService.deliverStatement("Good news! You are now eligible to apply for our investment products.", new EmailStatementSender());
-        // send via letter
-        statementDeliveryService.deliverStatement("Please be aware of fraud.", new LetterStatementSender());
+        LocalDate start = LocalDate.now().minusMonths(1);
+        LocalDate end = LocalDate.now();
+
+        // Send full statement via Email
+        statementService.sendAllTransactionsStatement("ACC-12345", "Mic Jones", start, end, new EmailStatementSender());
+        System.out.println("==================================================");
+
+        // Send outgoing only statement via letter
+        statementService.sendOutgoingTransactionsStatement("ACC-983838", "Barry Jones", start, end, new LetterStatementSender());
     }
 }
