4c7ce12698bcb5a3d30701043465f1918d9015ea
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-24 07:59:05 +0100

    add searching an item in a list

--- track/learning/src/main/java/learning/data_structures/LanguagesList.java
+++ track/learning/src/main/java/learning/data_structures/LanguagesList.java
@@ -1,11 +1,23 @@
 package learning.data_structures;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class LanguagesList {
 
+    // Search for an item in a list
+
+    private static <T> boolean searchForItemInList(T item, Collection<T> list){
+        if(list.contains(item)){
+            System.out.println(item + " is on the list");
+            return true;
+        }
+
+        System.out.println(item + " is not on the list");
+        return false;
+    }
 
     static void main(String[] args) {
         List<String> languages = new ArrayList<>();
@@ -34,5 +46,12 @@ public class LanguagesList {
         languages.remove(languages.size() - 1);
 
         System.out.println("After removing some languages: " + languages);
+
+        // Searching for elements
+        searchForItemInList("Java", languages);
+        searchForItemInList("C++", languages);
+        searchForItemInList(89, nums);
+        searchForItemInList(100, nums);
+
     }
 }
