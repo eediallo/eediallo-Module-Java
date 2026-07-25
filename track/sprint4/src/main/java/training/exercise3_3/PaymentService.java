3963a16cb31b4bc719481563785c8505a3c6dac0
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-25 18:19:44 +0100

    exercise 3.3: Update pseudocode to remove the try/catch block and handle the card/cash options more gracefully

---
+++ track/sprint4/src/main/java/training/exercise3_3/PaymentService.java
@@ -0,0 +1,19 @@
+package training.exercise3_3;
+/*
+public class PaymentService {
+    void processPayment(Payment payment) {
+
+        if (payment.isNull || payment.getAmount <= 0) {
+            throw new IllegalArgumentException("Invalid Payment details provided");
+        }
+
+        if (payment.hasCardDetails()) {
+            cardProcessor.process(payment);
+        } else if (payment.isCash()) {
+            cashProcessor.process(payment);
+        } else {
+            throw new UnsupportedPaymentMethodException("No valid payment method provided.");
+        }
+    }
+}
+*/
