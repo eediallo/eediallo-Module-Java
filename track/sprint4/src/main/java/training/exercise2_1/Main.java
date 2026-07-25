8b5471c8ac337614162ffe4c19bc563f1f393c60
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-25 08:45:01 +0100

    exercise 2.3: modifies collections to include new joiner

--- track/sprint4/src/main/java/training/exercise2_1/Main.java
+++ track/sprint4/src/main/java/training/exercise2_1/Main.java
@@ -14,17 +14,26 @@ public class Main {
 
         StudentGradeBook studentGradeBook = new StudentGradeBook(firstNames, lastNames, grades);
 
+        // New Joiner Data
+        String newJoinerFirstName = "Miriam";
+        String newJoinerLastName = "Barry";
+        int newJoinerGrade = 50;
+
         List<String> firstNamesList = studentGradeBook.getFirstNames();
+        firstNamesList.add(newJoinerFirstName);
         System.out.println("First Names: " + firstNamesList);
 
         Set<String> distinctFirstNames = studentGradeBook.getDistinctFirstNames();
+        distinctFirstNames.add(newJoinerFirstName);
         System.out.println("Distinct First Names: " + distinctFirstNames);
 
 
         Map<String, Integer> lastNameToGrades = studentGradeBook.getLastNameToGrade();
+        lastNameToGrades.put(newJoinerLastName, newJoinerGrade);
         System.out.println("LastName to grade: " + lastNameToGrades);
 
         Map<Student, Integer> studentToGrade = studentGradeBook.getStudentToGrade();
+        studentToGrade.put(new Student(newJoinerFirstName, newJoinerLastName), newJoinerGrade);
         studentToGrade.forEach((student, grade) -> {
             System.out.println(student + " -> Grade: " + grade);
         });
