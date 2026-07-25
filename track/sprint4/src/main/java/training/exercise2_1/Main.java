ec9fdb5fee9de2c1e6f4fec156b7c9b85d774653
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-25 08:56:33 +0100

    exercise 2.3: modifies collections to remove left student

--- track/sprint4/src/main/java/training/exercise2_1/Main.java
+++ track/sprint4/src/main/java/training/exercise2_1/Main.java
@@ -7,6 +7,11 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
+
+    public static void removeStudent(){
+
+    }
+
     public static void main(String[] args) {
         String[] firstNames = {"Anuket", "Eryn", "Filip", "Raquel", "Eryn"};
         String[] lastNames = {"Hassan", "Doe", "Pappas", "Clarke", "Jones"};
@@ -19,21 +24,30 @@ public class Main {
         String newJoinerLastName = "Barry";
         int newJoinerGrade = 50;
 
+        // Left student data
+        String leftStudentFirstName = "Filip";
+        String leftStudentLastName = "Pappas";
+        int leftStudentGrade = 35;
+
         List<String> firstNamesList = studentGradeBook.getFirstNames();
         firstNamesList.add(newJoinerFirstName);
+        firstNamesList.remove(leftStudentFirstName);
         System.out.println("First Names: " + firstNamesList);
 
         Set<String> distinctFirstNames = studentGradeBook.getDistinctFirstNames();
         distinctFirstNames.add(newJoinerFirstName);
+        distinctFirstNames.remove(leftStudentLastName);
         System.out.println("Distinct First Names: " + distinctFirstNames);
 
 
         Map<String, Integer> lastNameToGrades = studentGradeBook.getLastNameToGrade();
         lastNameToGrades.put(newJoinerLastName, newJoinerGrade);
+        lastNameToGrades.remove(leftStudentLastName);
         System.out.println("LastName to grade: " + lastNameToGrades);
 
         Map<Student, Integer> studentToGrade = studentGradeBook.getStudentToGrade();
         studentToGrade.put(new Student(newJoinerFirstName, newJoinerLastName), newJoinerGrade);
+        studentToGrade.remove(new Student(leftStudentFirstName, leftStudentLastName));
         studentToGrade.forEach((student, grade) -> {
             System.out.println(student + " -> Grade: " + grade);
         });
