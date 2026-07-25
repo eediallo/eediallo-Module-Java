1ea254751b0bf820a49e87c28a937d40efd89bf8
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-25 07:38:28 +0100

    exercise 2.2: implement getStudentToGrade method

--- track/sprint4/src/main/java/training/exercise2_2/Student.java
+++ track/sprint4/src/main/java/training/exercise2_2/Student.java
@@ -25,5 +25,14 @@ public class Student {
         return Objects.hash(firstName, lastName);
     }
 
+    Map<Student, Integer> getStudentToGrade(String[] firstNames, String[] lastNames, int[] grades) {
+        Map<Student, Integer> studentToGrade = new HashMap<>();
+
+        for (int i = 0; i < firstNames.length; i++) {
+            studentToGrade.put(new Student(firstNames[i], lastNames[i]), grades[i]);
+        }
+
+        return studentToGrade;
+    }
 
 }
