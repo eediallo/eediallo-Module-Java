87e25a35daffa904bcd2ada648b3922bb7c7e4c4
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-25 15:26:38 +0100

    exercise 3.1: add main method to demonstrate use of fileReaderService

---
+++ track/sprint4/src/main/java/training/exercise3_1/Main.java
@@ -0,0 +1,60 @@
+package training.exercise3_1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        FileReaderService fileReaderService = new FileReaderService();
+
+        String validFile = "valid.txt";
+        String emptyFile = "empty.txt";
+        String nonExistingFile = "non_existing.txt";
+
+        createDummyFiles(validFile, emptyFile);
+
+        System.out.println("READING VALID FILE...");
+        testFileReadingDemo(fileReaderService, validFile);
+
+        System.out.println("\nREADING EMPTY FILE...");
+        testFileReadingDemo(fileReaderService, emptyFile);
+
+        System.out.println("\nREADING NON EXISTING FILE...");
+        testFileReadingDemo(fileReaderService, nonExistingFile);
+
+        System.out.println("\nPASSING NULL FILE NAME...");
+        testFileReadingDemo(fileReaderService, emptyFile);
+
+        // remove dummy file
+        new File(validFile).delete();
+        new File(emptyFile).delete();
+    }
+
+
+    private static void testFileReadingDemo(FileReaderService fileReaderService, String validFile) {
+        try {
+            String content = fileReaderService.readFile(validFile);
+            System.out.println("SUCCESS: File Content:\n" + content);
+        } catch (IOException e) {
+            System.out.println("CAUGHT INVALID IO EXCEPTION: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("CAUGHT NULL ARGUMENT EXCEPTION: " + e.getMessage());
+        }
+
+    }
+
+    private static void createDummyFiles(String validFile, String emptyFile) {
+        try {
+            FileWriter fileWriter = new FileWriter(validFile);
+            fileWriter.write("Hello, World!\nThis file is for testing purpose only");
+            fileWriter.close();
+
+            // create empty file
+            File empty = new File(emptyFile);
+            empty.createNewFile();
+        } catch (IOException e) {
+            System.err.println("Could not create dummy files for test: " + e.getMessage());
+        }
+    }
+}
