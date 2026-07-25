d8d80777181a636951e57f21293d50182f53afbf
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-25 07:37:00 +0100

    exercise 2.2: implement Student class with overriden equals() and hashCode() methods.

---
+++ track/sprint4/src/main/java/training/exercise2_2/Student.java
@@ -0,0 +1,29 @@
+package training.exercise2_2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+
+    public  Student(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+
+}
