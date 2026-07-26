fe2f72c7ee0457476e3e2251c1a69a124fa1c810
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-26 10:23:34 +0100

    implement ThreeYearContractRentalGenerator

---
+++ track/sprint4-backlog/src/main/java/training/rental_summary_generator/ThreeYearContractRentalGenerator.java
@@ -0,0 +1,30 @@
+package training.rental_summary_generator;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreeYearContractRentalGenerator implements RentalCalculator {
+
+    @Override
+    public List<Rental> generateRentals(Contract contract) {
+        List<Rental> rentals = new ArrayList<>();
+
+        int totalMonths = 36;
+
+        double monthlyCapital = contract.getCarePrice() / totalMonths;
+        double monthlyInterest = (contract.getCarePrice() * 0.03) / totalMonths;
+        LocalDate today = LocalDate.now();
+
+        for (int i = 0; i < totalMonths; i++) {
+            // Rentals are due monthly starting 1 month after the start date
+            LocalDate dueDate = contract.getStartDate().plusMonths(i + 1);
+            boolean isPaid = !dueDate.isAfter(today);
+
+            rentals.add(new Rental(dueDate, monthlyCapital, monthlyInterest, isPaid));
+        }
+
+        return rentals;
+
+    }
+}
