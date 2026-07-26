89f02b96375b4f02152338b45d616ae9d6971045
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-26 07:47:17 +0100

    exercise 3.3: add Stream with map demo

---
+++ track/learning/src/main/java/learning/streams/StudentGrade.java
@@ -0,0 +1,19 @@
+package learning.streams;
+
+import java.util.List;
+import java.util.Map;
+
+public class StudentGrade {
+    static void main(String[] args) {
+        Map<String, Integer> m = Map.of("Mic", 90, "Salif", 100, "Miriam", 30);
+        m.entrySet().stream().forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+
+        int total = m.values().stream().mapToInt(Integer::intValue).sum();
+
+        List<String> studentList = m.keySet().stream().toList();
+
+        System.out.println("Sum of grades: " + total);
+
+        System.out.println("Student list: " + studentList);
+    }
+}
