18e9b3c0a2ce4fd833a891157bdf8389f6fb9022
eedialllo <elhhabdouldiallo@gmail.com>
2026-07-26 10:37:31 +0100

    add getters  and data retrieval method to the RentalSummary

--- track/sprint4-backlog/src/main/java/training/rental_summary_generator/RentalSummary.java
+++ track/sprint4-backlog/src/main/java/training/rental_summary_generator/RentalSummary.java
@@ -1,8 +1,37 @@
 package training.rental_summary_generator;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class RentalSummary {
     private List<Rental> rentals;
-    private List<Contract> contracts;
+    private Contract contract;
+
+    public RentalSummary(List<Rental> rentals, Contract contract) {
+        // keep rentals sorted by due date
+        rentals.sort(Comparator.comparing(Rental::getDueDate));
+        this.rentals = rentals;
+
+        this.contract = contract;
+    }
+
+    public List<Rental> getRentals() {
+        return rentals;
+    }
+
+    public Contract getContract() {
+        return contract;
+    }
+
+    public Rental getNextDueRental() {
+        return rentals.stream().filter(r -> !r.isPaid()).findFirst().orElse(null);
+    }
+
+    public double getTotalCapital() {
+        return rentals.stream().mapToDouble(Rental::getCapitalAmount).sum();
+    }
+
+    public double getTotalInterest() {
+        return rentals.stream().mapToDouble(Rental::getInterestAmount).sum();
+    }
 }
